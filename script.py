5d0a4f29d4434285ccd376e83b97500be3f451dd
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-03-19 21:37:25 +0900

    add lesson14

--- script.py
+++ script.py
@@ -63,3 +63,16 @@ elif money == apple_price:
   print("りんごを買うことができますが所持金が0になります")
 else:
   print("お金が足りません")
+
+
+x = 20
+if 10 <= x <= 30:
+  print("xは10以上30以下です")
+
+y = 60
+if 10 > y or y > 30:
+  print("yは10未満または30より大きいです")
+
+z = 55
+if not(z == 77):
+  print("zは77ではありません")
