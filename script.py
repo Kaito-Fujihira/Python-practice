2952605877195c543b9630faf44aa8ffc22e2d28
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-03-20 23:15:06 +0900

    add lesson15

--- script.py
+++ script.py
@@ -76,3 +76,10 @@ if 10 > y or y > 30:
 z = 55
 if not(z == 77):
   print("zは77ではありません")
+
+
+apple_price = 200
+count = 5
+total_price = apple_price * count
+print("購入するりんごの個数は" + str(count) + "個です")
+print("支払金額は" + str(total_price) + "円です")
