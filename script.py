3c16ed329246ac7bd4703084d7b7205b6b108c4c
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-03-20 23:28:52 +0900

    add lesson16

--- script.py
+++ script.py
@@ -83,3 +83,12 @@ count = 5
 total_price = apple_price * count
 print("購入するりんごの個数は" + str(count) + "個です")
 print("支払金額は" + str(total_price) + "円です")
+
+
+apple_price = 200
+input_count = input("購入するりんごの個数を入力してください：")
+count = int(input_count)
+total_price = apple_price * count
+
+print("購入するりんごの個数は" + str(count) + "個です")
+print("支払金額は" + str(total_price) + "円です")
