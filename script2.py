d32efd2a36a4e220c000e348b7b855d2e8c04100
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-03-22 12:14:54 +0900

    add leeson2-3

--- script2.py
+++ script2.py
@@ -1,4 +1,11 @@
-#lesson2-1
+#lesson2-2
 fruits = ["apple", "banana", "orange"]
 print(fruits[0])
 print("好きな果物は" + fruits[2] + "です")
+
+#lesson2-3
+fruits = ["apple", "banana", "orange"]
+fruits.append("grape")
+print(fruits)
+fruits[0] = "cherry"
+print(fruits[0])
