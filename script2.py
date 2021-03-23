c2efdaa280458aa683125ada7de59cd824af47fc
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-03-23 19:52:22 +0900

    add lesson2-6

--- script2.py
+++ script2.py
@@ -21,3 +21,10 @@ for fruit in fruits:
 fruits = {"apple": "りんご", "banana": "バナナ"}
 print(fruits["banana"])
 print("appleは" + fruits["apple"] + "という意味です")
+
+
+#lesson2-6
+fruits = {"apple": 100, "banana": 200, "orange": 400}
+fruits["banana"] = 300
+fruits["grape"] = 500
+print(fruits)
